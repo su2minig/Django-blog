d9d765ebf1d435d0f472832aea6ca7972f20adce
su <mini72667@gmail.com>
2023-11-02 12:39:11 +0900

    댓글작성CBV로 변경 및 댓글수정FBV로 구현

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,10 +1,12 @@
 from typing import Any
+from django.db import models
+from django.http import HttpRequest, HttpResponse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post, Comment
 from .forms import PostForm, CommentForm
 from django.urls import reverse_lazy
 from django.db.models import Q
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.decorators import login_required
 
@@ -37,34 +39,90 @@ class PostDetailView(DetailView):
 
     def get_object(self, queryset=None):
         pk = self.kwargs.get('pk')
-        # print(self.kwargs)
         post = Post.objects.get(pk=pk)
         post.view_count += 1
         post.save()
         return super().get_object(queryset)
     
 
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'blog/post.html'
+    
+    def form_valid(self, form):
+        post = Post.objects.get(pk=self.kwargs['pk'])
+        comment = form.save(commit=False) # commit=False는 DB에 저장하지 않고 객체만 반환
+        comment.post = post
+        comment.author = self.request.user
+        comment.save()
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse_lazy('blog:post', kwargs={'pk': self.kwargs['pk']})
+
+
+comment_create = CommentCreateView.as_view()
+
+# class CommentUpdateView(UserPassesTestMixin, UpdateView):
+#     model = Comment
+#     form_class = CommentForm
+
+#     def test_func(self):
+#         return self.get_object().author == self.request.user
+
+#     def get_success_url(self):
+#         return reverse_lazy('blog:post', kwargs = {'pk':self.object.post.pk})
+
+# comment_update = CommentUpdateView.as_view()
+
 @login_required
-def comment_create(request, pk):
+def comment_update(request, pk, comment_pk):
     post = Post.objects.get(pk=pk)
+    comment = Comment.objects.get(pk=comment_pk)
     if request.method == 'POST':
-        form = CommentForm(request.POST)
+        form = CommentForm(request.POST, instance=comment)
         if form.is_valid():
-            comment = form.save(commit=False) # commit=False는 DB에 저장하지 않고 객체만 반환
-            comment.post = post
-            comment.author = request.user
-            comment.save()
+            form.save()
             return redirect('blog:post', pk)
     else:
-        form = CommentForm()
-    return render(request, 'blog/post.html', {'form': form})
-
-def comment_delete(request, pk, comment_pk):
-    print(request)
-    comment = Comment.objects.get(pk=comment_pk)
-    print(comment)
-    comment.delete()
+        form = CommentForm(instance=comment)
     return redirect('blog:post', pk)
+
+# @login_required
+# def comment_create(request, pk):
+#     post = Post.objects.get(pk=pk)
+#     if request.method == 'POST':
+#         form = CommentForm(request.POST)
+#         if form.is_valid():
+#             comment = form.save(commit=False) # commit=False는 DB에 저장하지 않고 객체만 반환
+#             comment.post = post
+#             comment.author = request.user
+#             comment.save()
+#             return redirect('blog:post', pk)
+#     else:
+#         form = CommentForm()
+#     return render(request, 'blog/post.html', {'form': form})
+
+class CommentDeleteView(UserPassesTestMixin, DeleteView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'blog/post.html'
+
+    def test_func(self):
+        return self.get_object().author == self.request.user
+
+    def get_success_url(self):
+        return reverse_lazy('blog:post', kwargs = {'pk':self.kwargs['pk']})
+
+
+comment_delete = CommentDeleteView.as_view()
+
+# def comment_delete(request, pk, comment_pk):
+#     print(request)
+#     comment = Comment.objects.get(pk=comment_pk)
+#     comment.delete()
+#     return redirect('blog:post', pk)
 
 post = PostDetailView.as_view()
 
