80b7bbc20c06240cb610df4a6a77f9f56543cfd6
su <mini72667@gmail.com>
2023-11-05 13:33:16 +0900

    404에러페이지생성

--- blog/main/views.py
+++ blog/main/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def main(request):
     return render(request, 'main/main.html')
+
+def custom_404(request, exception):
+    return render(request, '404.html', status=404)
--- blog/makeblog/urls.py
+++ blog/makeblog/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.conf.urls import handler404
 from django.conf import settings
+
+
 
 urlpatterns = [
     path('', include('main.urls')),
@@ -9,5 +12,7 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('accounts/', include('accounts.urls')),
 ]
+
+handler404 = 'main.views.custom_404'
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
